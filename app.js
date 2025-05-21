7ac20d9eb06ecb23165ed851d75096a31cf6fe81
vitalya <vitalay.efimov@gmail.com>
2025-05-21 22:20:07 +0500

    слайд готов

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
-const slides = document.querySelectorAll('.slide');
 
-for (const slide of slides){
-    slide.addEventListener('click', () => {
-      clearActiveClasses();
-      slide.classList.add('active');
-    })
+  function slidePlugins (activeSlide){
+    const slides = document.querySelectorAll('.slide');
+    slides[activeSlide].classList.add('active')
+
+    for (const slide of slides) {
+      slide.addEventListener('click', () => {
+        clearActiveClasses();
+        slide.classList.add('active');
+      })
+    }
+
+    function clearActiveClasses() {
+      // for (const slide of slides){
+      //   slide.classList.remove('active');
+      // }
+      slides.forEach(slide => slide.classList.remove('active'));
+    }
   }
-        
-  function clearActiveClasses(){
-    // for (const slide of slides){
-    //   slide.classList.remove('active');
-    // }
-    slides.forEach(slide => slide.classList.remove('active'));
-  }
+slidePlugins(0)
